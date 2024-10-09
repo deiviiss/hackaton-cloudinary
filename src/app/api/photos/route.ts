fec4a727122733e9d47f3115cf8d570f09e3d1d5
ArceDanielShok <jarce@shokworks.io>
2024-10-09 14:14:07 -0300

    init task

--- src/app/api/photos/route.ts
+++ src/app/api/photos/route.ts
@@ -1,11 +1,38 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
 	try {
-		// toda la logica de tu endpoint
-		return { status: 200, message: 'ok' }
-	} catch (error) {
-		console.error(error)
-		return { error: `${error}` }
+		const formData = await request.formData()
+		const image = formData.get('image')
+		const theme = formData.get('theme')
+		const description = formData.get('description')
+
+		if (!image || !theme || !description) {
+			return NextResponse.json(
+				{ error: 'All fields are required' },
+				{ status: 400 },
+			)
+		}
+
+		// const cloudinaryResult: any = await uploadImageToCloudinary(image)
+		// const imageUrl = cloudinaryResult.secure_url
+
+		// const processedImageUrl = await processImageWithGenBackground(
+		// 	imageUrl,
+		// 	theme,
+		// )
+
+		// 3. Responder con la URL de la imagen procesada
+		return NextResponse.json(
+			{
+				// imageId: cloudinaryResult.public_id,
+				// processedImageUrl: processedImageUrl,
+				description: description,
+			},
+			{ status: 200 },
+		)
+	} catch (error: unknown) {
+		console.error('Error in POST /api/v1/upload:', error)
+		return NextResponse.json({ error: 'An error occurred' }, { status: 500 })
 	}
 }
