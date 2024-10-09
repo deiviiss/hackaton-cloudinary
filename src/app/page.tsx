e928843dea1273c7771a719b78799348c4e41f2d
lags2022 <lguzman.58erb@outlook.com>
2024-10-08 20:51:43 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { useEffect } from 'react'
-
 export default function Home() {
-	useEffect(() => {
-		fetch('/api/prueba-borrar', {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-	}, [])
 	return <h1>Test</h1>
 }
